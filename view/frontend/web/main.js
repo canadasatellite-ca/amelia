c35985f281a2877f05f633d08ec37d2066bdd046
Dmitry Fedyuk <dfediuk@gmail.com>
2021-09-23 23:58:55 +0600

    https://github.com/canadasatellite-ca/amelia/issues/1

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -6,13 +6,22 @@ define(['jquery'], function($) {return (
 	 * @returns void
 	 */
 	function(c) {
-		// 2021-09-23
-		// Creating the IFRAME on the server side (in the \CanadaSatellite\Amelia\Block::_toHtml() method)
-		// breaks the Magento's JavaScripts for an unknown reason, so I create the IFRAME on the client's side.
-		var $b = $('<div>').addClass('cs-amelia')
-			.append($('<div>').addClass('cs-close').text('✕'))
-			.append($('<iframe>').attr({height: '100%', src: c.url, width: '100%'}))
-		;
-		$('body').append($b);
+		(function() {
+			var $globe = $('<div>');
+			var $b = $('<button>').addClass('cs-amelia-closed').append($globe);
+			$('body').append($b);
+		})();
+		(function() {
+			// 2021-09-23
+			// Creating the IFRAME on the server side (in the \CanadaSatellite\Amelia\Block::_toHtml() method)
+			// breaks the Magento's JavaScripts for an unknown reason, so I create the IFRAME on the client's side.
+			var $b = $('<div>').addClass('cs-amelia-open df-hidden');
+			var $close = $('<div>').addClass('cs-close').text('✕').click(function() {$b.hide();});
+			$b
+				.append($close)
+				.append($('<iframe>').attr({height: '100%', src: c.url, width: '100%'}))
+			;
+			$('body').append($b);
+		})();
 	});
 });
