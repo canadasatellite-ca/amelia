d13c75bdf5a0684f045fe4d49a984b953e5a89db
Dmitry Fedyuk <dfediuk@gmail.com>
2021-09-23 21:55:03 +0600

    https://github.com/canadasatellite-ca/amelia/issues/1

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -6,6 +6,9 @@ define(['jquery'], function($) {return (
 	 * @returns void
 	 */
 	function(c) {
+		// 2021-09-23
+		// Creating the IFRAME on the server side (in the \CanadaSatellite\Amelia\Block::_toHtml() method)
+		// breaks the Magento's JavaScripts for an unknown reason, so I create the IFRAME on the client's side.
 		var $b = $('<div>').addClass('cs-amelia').html($('<iframe>').attr({height: '100%', src: c.url, width: '100%'}));
 		$('body').append($b);
 	});
